0993cf4da577134df136812b76a26fe6270daab5
Paul Broadwith <paul@pauby.com>
2018-08-18 21:45:03 +0100

    Fix issues with client OS

--- scripts/PrepareWindows.ps1
+++ scripts/PrepareWindows.ps1
@@ -1,14 +1,24 @@
 # Adapted from http://stackoverflow.com/a/29571064/18475
 $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
 $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
-New-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force | Out-Null
-New-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force | Out-Null
+if (Test-Path $AdminKey) {
+    New-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force | Out-Null
+    Write-Output "IE Enhanced Security Configuration (ESC) has been disabled for Admin."
+}
+
+if (Test-Path $UserKey) {
+    New-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force | Out-Null
+    Write-Output "IE Enhanced Security Configuration (ESC) has been disabled for User."
+}
+
 #Stop-Process -Name Explorer -Force
-Write-Output "IE Enhanced Security Configuration (ESC) has been disabled."
 
 # http://techrena.net/disable-ie-set-up-first-run-welcome-screen/
-New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize" -Value 1 -PropertyType "DWord" -Force | Out-Null
-Write-Output "IE first run welcome screen has been disabled."
+$key = "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main"
+if (Test-Path $key) {
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize" -Value 1 -PropertyType "DWord" -Force | Out-Null
+    Write-Output "IE first run welcome screen has been disabled."
+}
 
 Write-Output 'Setting Windows Update service to Manual startup type.'
 Set-Service -Name wuauserv -StartupType Manual
