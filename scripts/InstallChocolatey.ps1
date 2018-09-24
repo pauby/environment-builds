2af2216ab416d2b616e6dc761ecc0e13b93743b7
Paul Broadwith <paul@pauby.com>
2018-09-24 16:38:01 +0100

    Add code to notify GUI apps

--- scripts/InstallChocolatey.ps1
+++ scripts/InstallChocolatey.ps1
@@ -5,8 +5,10 @@ Param (
 )
 
 # From https://chocolatey.org/install
+$installScript = Join-Path -Path $env:TEMP -ChildPath "$(([GUID]::NewGuid()).Guid.ToString()).ps1"
 Set-ExecutionPolicy Bypass -Scope Process -Force
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Invoke-WebRequest 'https://chocolatey.org/install.ps1' -UseBasicParsing -OutFile $installScript
+& $installScript
 
 #Update-SessionEnvironment
 choco feature enable --name="'autouninstaller'"
@@ -29,3 +31,28 @@ if ($UseLocalSource.IsPresent) {
     choco source remove --name="'chocolatey'"
     choco source add --name='chocolatey' --source='https://chocolatey.org/api/v2/' --priority='2' --bypass-proxy
 }
+
+# Notify Gui apps of environment change
+# because sometimes explorer.exe just doesn't get the message that things were updated.
+if (-not ("win32.nativemethods" -as [type])) {
+    # import sendmessagetimeout from win32
+    add-type -Namespace Win32 -Name NativeMethods -MemberDefinition @"
+[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+public static extern IntPtr SendMessageTimeout(
+  IntPtr hWnd, uint Msg, UIntPtr wParam, string lParam,
+  uint fuFlags, uint uTimeout, out UIntPtr lpdwResult);
+"@
+}
+
+$HWND_BROADCAST = [intptr]0xffff;
+$WM_SETTINGCHANGE = 0x1a;
+$result = [uintptr]::zero
+
+# notify all windows of environment block change
+[win32.nativemethods]::SendMessageTimeout($HWND_BROADCAST, $WM_SETTINGCHANGE, [uintptr]::Zero, "Environment", 2, 5000, [ref]$result);
+
+# Attempting to make the script recapture environment changes
+setx.exe trigger 1
+
+#Write-Output "For good measure, we are going to take out explorer"
+#tskill.exe explorer /a /v
