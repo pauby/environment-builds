65aa1301ef689beed62b9bceaf8ae69d61d60181
Paul Broadwith <paul@pauby.com>
2019-02-19 09:21:44 +0000

    Add calling choco package install script

--- scripts/InstallStandardApps.ps1
+++ scripts/InstallStandardApps.ps1
@@ -1,8 +1,16 @@
-if ($null -eq (Get-Command -Name 'choco.exe' -ErrorAction SilentlyContinue)) {
-    Write-Warning "Chocolatey not installed. Cannot install standard packages."
+# only install these apps on an operating system that has Explorer (ie. not Server Core)
+if ([bool](Get-Command -Name 'explorer.exe' -ErrorAction SilentlyContinue)) {
+    & \scripts\InstallChocoPackage.ps1 -Package @( 'baretail', 'dotnetversiondetector', 'notepadplusplus.install' )
 }
-else {
-    @( 'baretail', 'dotnetversiondetector', 'notepadplusplus.install', '7zip' ) | ForEach-Object {
-        choco upgrade $_ -y --no-progress
-    }
-}
+
+# if ($null -eq (Get-Command -Name 'choco.exe' -ErrorAction SilentlyContinue)) {
+#     Write-Warning "Chocolatey not installed. Cannot install standard packages."
+# }
+# else {
+#     @( 'baretail', 'dotnetversiondetector', 'notepadplusplus.install', '7zip' ) | ForEach-Object {
+#         choco upgrade $_ -y --no-progress
+#     }
+# }
+
+# install in all environments
+& \scripts\InstallChocoPackage.ps1 -Package @( '7zip', 'git' )
