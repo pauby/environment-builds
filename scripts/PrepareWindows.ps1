8a7b891e12803ea3032c180b3fad5933846d3d64
Paul Broadwith <paul@pauby.com>
2019-03-16 23:02:31 +0000

    Null output, stop windows services and remove appx packages

--- scripts/PrepareWindows.ps1
+++ scripts/PrepareWindows.ps1
@@ -24,14 +24,14 @@ if ([bool](Get-Command -Name 'iexplore.exe'  -ErrorAction SilentlyContinue)) {
     }
 }
 
-Write-Output 'Setting Windows Update service to Manual startup type.'
-Set-Service -Name wuauserv -StartupType Manual
+Write-Output 'Stopping Windows Update service and setting to Manual startup type.'
+Stop-Service wuauserv -Passthru | Set-Service -StartupType Manual
 
 # Ensure there is a profile file so we can get tab completion
-New-Item -ItemType Directory $(Split-Path $profile -Parent) -Force
+$null = New-Item -ItemType Directory $(Split-Path $profile -Parent) -Force
 Set-Content -Path $profile -Encoding UTF8 -Value "" -Force
 
-winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="2048"}'
+$null = winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="2048"}'
 
 # Server Core does not have Explorer.exe
 if ([bool](Get-Command -Name 'explorer.exe'  -ErrorAction SilentlyContinue)) {
@@ -98,4 +98,31 @@ catch {
 if ([bool](get-Command -Name 'servermanager.exe' -ErrorAction SilentlyContinue) -and $osData.ProductType -eq 3) {
     Write-Host 'Disabling Server Manager for starting at login.'
     Get-ScheduledTask -TaskName 'ServerManager' | Disable-ScheduledTask | Out-Null
+}
+
+Write-Output "Disabling non-essential Windows services."
+'ajrouter', 'alg', 'appmgmt', 'appxsvc', 'bthavctpsvc', 'bits', 'btagservice', 'bthserv',
+    'bluetoothuserservice_*', 'peerdistsvc', 'captureservice_*', 'certpropsvc', 'nfsclnt',
+    'diagtrack', 'IpxlatCfgSvc', 'iphlpsvc', 'SharedAccess', 'irmon', 'vmicvss', 'vmictimesync',
+    'vmicrdv', 'vmicvmsession', 'vmicheartbeat', 'vmicshutdown', 'vmicguestinterface', 'vmickvpexchange',
+    'HvHost', 'lfsvc', 'MapsBroker', 'dmwappushsvc', 'PhoneSvc', 'SEMgrSvc', 'WpcMonSvc', 'CscService',
+    'NcdAutoSetup', 'Netlogon', 'NaturalAuthentication', 'SmsRouter', 'MSiSCSI', 'AppVClient', 'SNMPTRAP',
+    'SCPolicySvc', 'ScDeviceEnum', 'SCardSvr', 'SensorService', 'SensrSvc', 'SensorDataService', 'RetailDemo',
+    'icssvc', 'WMPNetworkSvc', 'wisvc', 'wcncsvc', 'FrameServer', 'WFDSConSvc', 'WebClient', 'TabletInputService',
+    'XboxNetApiSvc', 'XblGameSave', 'XblAuthManager', 'xbgm', 'WwanSvc', 'wuauserv' | ForEach-Object {
+    Stop-Service -Name $_ -PassThru -ErrorAction SilentlyContinue | Set-Service -StartupType Disabled -ErrorAction SilentlyContinue
+}
+
+Write-Output "Stopping OneDrive."
+Get-Process -Name OneDrive* | Stop-Process -ErrorAction SilentlyContinue 
+
+if (Get-Command -Command 'Get-AppxPackage') {
+    '*3dbuilder*', '*alarms*', '*appconnector*', '*appinstaller*', '*communicationsapps*', '*calculator*', '*camera*',
+        '*feedback*', '*officehub*', '*getstarted*', '*skypeapp*', '*zunemusic*', '*zune*', '*maps*', '*messaging*',
+        '*solitaire*', '*wallet*', '*connectivitystore*', '*bingfinance*', '*bing*', '*zunevideo*', '*bingnews*', 
+        '*onenote*', '*oneconnect*', '*mspaint*', '*people*', '*commsphone*', '*windowsphone*', '*phone*', '*photos*',
+        '*bingsports*', '*sticky*', '*sway*', '*3d*', '*soundrecorder*', '*bingweather*', '*holographic*',
+        '*windowsstore*', '*xbox*' | ForEach-Object {
+        Get-AppxPackage -Name $_ -AllUsers -ErrorAction SilentlyContinue | Remove-AppxPackage -Package $_ -ErrorAction SilentlyContinue
+    }
 }
