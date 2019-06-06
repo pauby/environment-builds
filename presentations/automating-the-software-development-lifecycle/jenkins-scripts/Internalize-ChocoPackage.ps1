c75f142b687970efbd6f2a559770e961ea19e1e8
Paul Broadwith <paul@pauby.com>
2019-06-06 12:18:01 +0100

    Add notes, removed commented code and Jenkins code

--- presentations/automating-the-software-development-lifecycle/jenkins-scripts/Internalize-ChocoPackage.ps1
+++ presentations/automating-the-software-development-lifecycle/jenkins-scripts/Internalize-ChocoPackage.ps1
@@ -1,3 +1,8 @@
+<#
+.NOTES
+    Written by Paul Broadwith (paul@pauby.com) October 2018
+#>
+
 [CmdletBinding()]
 Param (
     [Parameter(Mandatory)]
@@ -34,10 +39,10 @@ if (Test-Path -Path $OutputDirectory) {
 }
 New-Item -Path $OutputDirectory -ItemType Directory -Force
 
+# Go through each package name provided and internalize it
 $params = "--internalize --internalize-all-urls --append-use-original-location --source=""'https://chocolatey.org/api/v2/; https://licensedpackages.chocolatey.org/api/v2/'"" --no-progress --limitoutput"
 $Name | ForEach-Object {
     if ($Force.IsPresent -or (-not (Test-PackageExist -Name $_ -Version $Version -PackagePath $OutputDirectory))) {
-#    if ($Force.IsPresent -or (-not (Test-Path -Path (Join-Path -Path $OutputDirectory -ChildPath "$_.*.nupkg")))) {
         Write-Host "Internalizing package '$_' from the Chocolatey Community Repository." -ForegroundColor Green
         $cmd = "choco download $_ --output-directory=$OutputDirectory $params "
         if ($Force.IsPresent) {
@@ -51,24 +56,3 @@ $Name | ForEach-Object {
         Write-Warning "To internalize this package anyway, use the -Force parameter."
     }
 }
-
-# Jenkins Job Code
-# node {
-#     powershell '''
-#         Set-Location (Join-Path -Path $env:SystemDrive -ChildPath 'scripts')
-#          $temp = Join-Path -Path $env:TEMP -ChildPath ([GUID]::NewGuid()).Guid
-#          New-Item -Path $temp -ItemType Directory | Out-Null
-#          Write-Output "Created temporary directory '$temp'."
-         
-#         .\\Internalize-ChocoPackage.ps1 `
-#             -Name $env:P_PKG_LIST `
-#             -OutputDirectory $temp `
-#             -Verbose
-        
-#         if ($LASTEXITCODE -eq 0) {
-#             Set-Location -Path $temp
-#             Get-ChildItem -Path *.nupkg | ForEach-Object {
-#                 choco push $_.Name --source=$env:P_DST_URL --apikey=$env:P_API_KEY --limitoutput
-#             }
-#         }    '''
-# }
