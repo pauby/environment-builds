e63d9ab16550a9ff7ce75bd334a57e79163a9cf1
Paul Broadwith <paul@pauby.com>
2019-07-14 20:31:23 +0100

    Add version and localsource switches

--- shell/InstallChocoPackage.ps1
+++ shell/InstallChocoPackage.ps1
@@ -4,7 +4,10 @@ Param (
     $Name,
 
     [string]
-    $Version
+    $Version,
+
+    [switch]
+    $UseLocalSource
 )
 
 if ($null -eq (Get-Command -Name 'choco.exe' -ErrorAction SilentlyContinue)) {
@@ -12,9 +15,13 @@ if ($null -eq (Get-Command -Name 'choco.exe' -ErrorAction SilentlyContinue)) {
 }
 else {
     $Name | ForEach-Object {
-        $cmd = "choco upgrade $_ -y --no-progress"
+        $cmd = "choco upgrade $_ -y --no-progress --limit-output"
         if ($Version) {
             $cmd += " --version $Version"
+        }
+
+        if ($UseLocalSource.IsPresent) {
+            $cmd += " --source c:\packages"
         }
 
         Write-Output "Installing Chocolatey package '$_' with command '$cmd'."
