996f1337bfe2bc620099bf4f3618583143b2fab3
Paul Broadwith <paul@pauby.com>
2019-07-21 20:40:54 +0100

    Fix typo

--- boxstarter/tests/startbs.ps1
+++ boxstarter/tests/startbs.ps1
@@ -1,4 +1,4 @@
-Remove-Item -Path 'c:\vagrant\reboot.flag' -ErrorAction SilentylContinue
+Remove-Item -Path 'c:\vagrant\reboot.flag' -ErrorAction SilentlyContinue
 $secpasswd = ConvertTo-SecureString "vagrant" -AsPlainText -Force
 $mycreds = New-Object System.Management.Automation.PSCredential ("vagrant", $secpasswd)
 Install-BoxstarterPackage -PackageName c:\vagrant\tests\boxstarter-test.ps1 -Credential $mycreds
