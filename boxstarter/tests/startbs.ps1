2f19bbaf7a191474758a3372268d407bdb5d1dae
Paul Broadwith <paul@pauby.com>
2019-07-21 19:42:51 +0100

    Remove reboot flag

--- boxstarter/tests/startbs.ps1
+++ boxstarter/tests/startbs.ps1
@@ -1,3 +1,4 @@
+Remove-Item -Path 'c:\vagrant\reboot.flag' -ErrorAction SilentylContinue
 $secpasswd = ConvertTo-SecureString "vagrant" -AsPlainText -Force
 $mycreds = New-Object System.Management.Automation.PSCredential ("vagrant", $secpasswd)
 Install-BoxstarterPackage -PackageName c:\vagrant\tests\boxstarter-test.ps1 -Credential $mycreds
