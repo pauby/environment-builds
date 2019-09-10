52893041e6bfd3e613f696b9455fb38fdf5324b9
Paul Broadwith <paul@pauby.com>
2019-09-10 22:05:20 +0100

    Change provision scripts to submodule

--- shell/InstallBGInfo.ps1
+++
@@ -1,4 +0,0 @@
-choco upgrade bginfo
-
-Copy-Item "\shell\bginfo.bgi" 'C:\programdata\chocolatey\lib\bginfo\tools\bginfo.bgi'
-Set-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run -Name bginfo -Value 'c:\programdata\chocolatey\lib\bginfo\tools\bginfo.exe C:\programdata\chocolatey\lib\bginfo\tools\bginfo.bgi /accepteula /silent /timer:0'
--- shell/InstallVMGuestTools.ps1
+++
@@ -1,7 +0,0 @@
-$provider = (Get-WmiObject -Class Win32_ComputerSystem).Model
-
-switch ($provider) {
-    "virtualbox" {
-        choco upgrade virtualbox-guest-additions-guest.install --version 6.0.8 -y
-    }
-}
