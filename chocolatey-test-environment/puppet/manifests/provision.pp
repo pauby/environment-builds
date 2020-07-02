6cdde35a44536943d71bb4f645ab62994f2ff69b
Paul Broadwith <paul@pauby.com>
2020-07-02 09:37:54 +0100

    fix: Add Windows Update service state

--- chocolatey-test-environment/puppet/manifests/provision.pp
+++ chocolatey-test-environment/puppet/manifests/provision.pp
@@ -1,5 +1,9 @@
+class { 'pauby_vagrant_provision::win_updates':
+  ensure => 'manual',
+}
+
 # The Chocolatey provider does not have the ability to install from a local
 # nupkg so install it first here.
-include chocolatey_local_install
+include pauby_vagrant_provision::win_chocolatey_configuration_standard
 
-include chocolatey_configure
+include pauby_vagrant_provision::win_chocolatey_source_local_add
