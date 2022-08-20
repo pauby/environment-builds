8e658534017c427c7044364b9b647bfce84ce410
Paul Broadwith <paul@pauby.com>
2022-08-20 21:24:34 +0100

    (enhance) Add OS specific config

--- choco-builds/puppet/manifests/provision.pp
+++ choco-builds/puppet/manifests/provision.pp
@@ -1,11 +1,23 @@
-# The Chocolatey provider does not have the ability to install from a local
-# nupkg so install it first here.
-include chocolatey_local_install
+include pauby_vagrant_provision
 
-include chocolatey_configure
+include pauby_vagrant_provision::win_os_provision
+
+include pauby_vagrant_provision::win_chocolatey_configuration_standard
+
+include pauby_vagrant_provision::win_chocolatey_source_local_add
+
+# disable the Chocolatey source
+chocolateysource { 'chocolatey':
+  ensure => disabled,
+}
+
+case $operatingsystem {
+  'windows': {
+    Package { provider => chocolatey, }
+  }
+}
 
 $packages_install = [ "notepadplusplus", "7zip" ]
 package { $packages_install:
-  ensure   => installed,
-  provider => "chocolatey",
+  ensure => installed,
 }
